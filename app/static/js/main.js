0ee3f577d9ed003ab00b3fce019ec88510d86a78
Søren Fuglede Jørgensen <s@fuglede.dk>
2020-03-01 14:52:35 +0100

    Fix indentation

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -2,35 +2,35 @@ function updateGreenestHour() {
     var period = $('#dropdown-toggle-period').data('value');
     var horizon = $('#dropdown-toggle-horizon').data('value');
     $.get("/api/v1/greenest-hour/" + period + "/" + horizon, function(data) {
-      $("#current-intensity").text(data["current-intensity"]);
-      $("#improvement").text(data["improvement"]);
-      $("#best-hour-start").text(data["best-hour-start"]);
-      $("#best-hour-end").text(data["best-hour-end"]);
-      $("#best-hour-intensity").text(data["best-hour-intensity"]);
+        $("#current-intensity").text(data["current-intensity"]);
+        $("#improvement").text(data["improvement"]);
+        $("#best-hour-start").text(data["best-hour-start"]);
+        $("#best-hour-end").text(data["best-hour-end"]);
+        $("#best-hour-intensity").text(data["best-hour-intensity"]);
     });
 }
 
 function updateEmissionIntensity() {
     $.get("/api/v1/current-emission-intensity", function(data) {
-      $("#jumbotron").css("background-color", data["intensity-level-bgcolor"]);
-      $("#jumbotron").css("color", data["intensity-level-fgcolor"]);
-      $("#info-link").css("color", data["intensity-level-fgcolor"]);
-      $(".btn-transparent").css('border-color', data["intensity-level-border-color"]);
-      $("#intensity-level").text(data["intensity-level"]);
-      $("#latest-data").text(data["latest-data"]);
-      $("#body").css('visibility', 'visible');
-      $(".btn-transparent").css('color', data["intensity-level-fgcolor"]);
-      var length = data["forecast-length-hours"];
-      if (length <= 12) {
-        $("#dropdown-12-hours").html(length + " timer");
-        $("#dropdown-24-hours").css('visibility', 'collapse');
-      } else if (length < 24) {
-        $("#dropdown-24-hours").html(length + " timer");
-      }
+        $("#jumbotron").css("background-color", data["intensity-level-bgcolor"]);
+        $("#jumbotron").css("color", data["intensity-level-fgcolor"]);
+        $("#info-link").css("color", data["intensity-level-fgcolor"]);
+        $(".btn-transparent").css('border-color', data["intensity-level-border-color"]);
+        $("#intensity-level").text(data["intensity-level"]);
+        $("#latest-data").text(data["latest-data"]);
+        $("#body").css('visibility', 'visible');
+        $(".btn-transparent").css('color', data["intensity-level-fgcolor"]);
+        var length = data["forecast-length-hours"];
+        if (length <= 12) {
+            $("#dropdown-12-hours").html(length + " timer");
+            $("#dropdown-24-hours").css('visibility', 'collapse');
+        } else if (length < 24) {
+            $("#dropdown-24-hours").html(length + " timer");
+        }
 
-      var plot_data = data["plot-data"];
-      var plot_opt = {"renderer": "canvas", "actions": false};
-      vegaEmbed("#vis", plot_data, plot_opt);
+        var plot_data = data["plot-data"];
+        var plot_opt = {"renderer": "canvas", "actions": false};
+        vegaEmbed("#vis", plot_data, plot_opt);
     });
 }
 
