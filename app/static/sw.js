dafb0abbf754765e6a986ce57086bc3404fe06ff
Søren Fuglede Jørgensen <s@fuglede.dk>
2020-03-21 21:45:49 +0100

    Close notification and open app on notification click
    This closes https://github.com/fuglede/groenstroem-web/issues/42

--- app/static/sw.js
+++ app/static/sw.js
@@ -13,6 +13,14 @@ const showLocalNotification = (title, body, swRegistration) => {
     swRegistration.showNotification(title, options);
 };
 
+self.addEventListener('notificationclick', function(event) {
+    // When the puh notification is clicked, remove it, and open the app.
+    const clickedNotification = event.notification;
+    clickedNotification.close();
+    const promiseChain = clients.openWindow('/');
+    event.waitUntil(promiseChain);
+});
+
 // Chrome unfortunately requires a fetch event handler to be in place for an app to be
 // considered "installable", even if there is no reason to handle the event. We get around
 // this by simply adding a trivial handler.
